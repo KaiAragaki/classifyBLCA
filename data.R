4a8593fdff2611b75c8fd13f98cb7392d3ca565d
Kai Aragaki <adamaragaki@gmail.com>
2022-10-27 19:36:40 -0400

    document

---
+++ data.R
@@ -0,0 +1,26 @@
+#' Centroids for the BLCA Consensus Classes
+#' 
+#' A data.frame containing the centroid coordinates for the six consensus classes of
+#' muscle-invasive bladder cancer
+#'
+#' @author Aurelie Kamoun
+#' @references \url{https://www.biorxiv.org/content/10.1101/488460v2}
+"centroids_consensus"
+
+#' Centroids for the BLCA UROMOL2021 Classes
+#' 
+#' A data.frame containing the centroid coordinates for the four UROMOL2021 classes of
+#' non-muscle-invasive bladder cancer
+#'
+#' @author Sia Lindskrog
+#' @references \url{https://www.nature.com/articles/s41467-021-22465-w}
+"centroids_uromol2021"
+
+#' Centroids for the BLCA TCGA Classes
+#' 
+#' A data.frame containing the centroid coordinates for the five TCGA classes of
+#' muscle-invasive bladder cancer
+#'
+#' @author Gordon Robertson
+#' @references \url{https://doi.org/10.1016/j.cell.2017.09.007}
+"centroids_tcga"
